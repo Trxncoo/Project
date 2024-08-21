05da3a89d1c79c2a8e3922713c3e1e4c928a12ca
Trxncoo <tramoscont@gmail.com>
2024-08-21 17:09:23 +0100

    [Added] Icon Component

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TextComponent } from './components/text/text.component';
+import { IconComponent } from './components/icon/icon.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TextComponent],
+  imports: [TextComponent, IconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ web-app/src/app/components/icon/icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, computed, input } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon.component.html',
+  styleUrl: './icon.component.scss',
+})
+export class IconComponent {
+  name = input<string>('');
+  color = input<string>('black');
+
+  iconClass = computed(() => `icon material-symbols-outlined`);
+}
