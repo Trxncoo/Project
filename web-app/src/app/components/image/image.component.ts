6111c2c49c0512ee88323d55e40b0ed0bcd7873d
Trxncoo <tramoscont@gmail.com>
2024-08-22 18:02:36 +0100

    [Added] Image Component

---
+++ web-app/src/app/components/image/image.component.ts
@@ -0,0 +1,15 @@
+import { Component, computed, input } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './image.component.html',
+  styleUrl: './image.component.scss',
+})
+export class ImageComponent {
+  path = input<string>('');
+  size = input<number>(0);
+
+  imageClass = computed(() => `image`);
+}
