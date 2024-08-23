460b3cf0b0174116b37f4b7b3a043ec2b13da1ee
Trxncoo <tramoscont@gmail.com>
2024-08-23 12:21:05 +0100

    [Added] Init Forgot Password Form

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { StringInputComponent } from './components/string-input/string-input.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ButtonComponent } from './components/button/button.component';
-import { TextComponent } from './components/text/text.component';
-import { LoginFormComponent } from './components/login-form/login-form.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RouterOutlet } from '@angular/router';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [StringInputComponent, ButtonComponent, TextComponent, LoginFormComponent, LoginPageComponent],
+  imports: [RouterOutlet, NotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
